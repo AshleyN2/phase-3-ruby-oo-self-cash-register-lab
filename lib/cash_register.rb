d0829da6dbb3434250012c56bd140f8b7d33a76e
Ashley <ashleynjeri7@gmail.com>
2022-10-24 19:10:25 +0300

    first commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :discount
+
+    def initialize (discount)
+        @discount = discount
+        @total = 0
+    end
+
+    def total 
+        return @total
+    end
+
+    def add_item (title, price)
+        
+    end
+end
