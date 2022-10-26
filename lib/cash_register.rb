1dd63f404b8774128c81f69fc689eee193dd4514
Ashley <ashleynjeri7@gmail.com>
2022-10-26 11:55:49 +0300

    passed tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,18 +1,37 @@
 require 'pry'
 
 class CashRegister
-    attr_accessor :discount
+    attr_accessor :discount, :total, :items, :last_transaction
 
-    def initialize (discount)
+    def initialize (discount = 0)
         @discount = discount
         @total = 0
+        @items = []
+        @last_transaction = 0
     end
 
     def total 
         return @total
     end
 
-    def add_item (title, price)
-        
+    def add_item (title, price, quantity = 1)
+        self.last_transaction = price * quantity
+        self.total += self.last_transaction
+        quantity.times do
+            self.items << title
+        end
+    end
+
+    def apply_discount
+        if self.discount == 0
+           return "There is no discount to apply."
+        else 
+            self.total -= self.total * self.discount / 100
+            return "After the discount, the total comes to $#{self.total}."  
+        end
+    end
+
+    def void_last_transaction
+        self.total -= self.last_transaction
     end
 end
